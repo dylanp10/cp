0ce15d6836ad0e5e9f20db4b7ab85c41d37d4bf6
Dylan Pino <dylanvpino@gmail.com>
2026-07-24 15:00:14 -0400

    Add files via upload

---
+++ cf/cf_1a_theatresquare.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+
+    int n, m, a;
+    cin >> n >> m >> a;
+    int length = m/a+1;
+    int width = n/a+1;
+    cout << length*width;
+
+}
